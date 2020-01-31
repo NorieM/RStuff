1cbd402bc6a733ff07997de0cfe1b4e47eb998ae
Norie MacEwan <50264139+NorieM@users.noreply.github.com>
2020-01-31 15:17:08 +0000

    Added CLI instruction

--- server.r
+++ server.r
@@ -1,3 +1,7 @@
+# can be run from command line with following
+#
+#   r -e "shiny::runApp('C:/test/projects/rstuff/home', launch.browser=TRUE)"
+
 server <- function(input, output){
 
   options(shiny.maxRequestSize=30*1024^2) 
@@ -21,23 +25,47 @@ server <- function(input, output){
     data <- as.data.frame(rawData$Row[start:end], stringsAsFactors = FALSE)
   
     names(data) <- c("Row")
-  
+
     parsedData <- data %>%
-      mutate(Date = substring(Row,16,25), Time = substring(Row,27,34), Direction = substring(Row, 36, 37)) %>%
-      mutate(Speed = as.numeric(substring(Row,39,44)), Class = as.integer(substring(Row,78,79))) %>%
-      select(Date, Time, Direction, Speed, Class)
+	mutate(Date = as.POSIXct(substring(Row,16,25)), Time = format(floor_date(as.POSIXct(substring(Row, 27, 34 ), format="%H:%M:%S"), "15 mins"), "%H:%M"), Direction = substring(Row, 36, 37)) %>%
+	mutate(Speed = as.numeric(substring(Row,39,44)), Class = as.integer(substring(Row,78,79))) %>%
+	select(Date, Time, Direction, Speed, Class)  
+
+    #parsedData <- data %>%
+     # mutate(Date = substring(Row,16,25), Time = substring(Row,27,34), Direction = substring(Row, 36, 37)) %>%
+      #mutate(Speed = as.numeric(substring(Row,39,44)), Class = as.integer(substring(Row,78,79))) %>%
+      #select(Date, Time, Direction, Speed, Class)
 
   })
 
-  output$primary <- renderText({unique(theData()$Direction[1])})
-  output$secondary <- renderText({unique(theData()$Direction[3])})
+  output$directions <- renderDataTable({
+	dirA <- unique(theData()$Direction)[1]
+	dirB <- unique(theData()$Direction)[2]
 
-  output$data <- renderTable({
+	dirs <- data.frame("Directions" = c(dirA, dirB))
+	})
+
+  output$data <- renderDataTable({
   
     classCount <- theData() %>%
-      group_by(Direction) %>%
+      group_by(Date,Direction) %>%
       count(Class) 
     
   })
-    
+
+  output$chart <- renderPlot({
+	chartData<-theData() %>%
+	mutate(Day = wday(Date, label=TRUE)) %>%
+	group_by(Day, Time) %>%
+	count(Time) %>%
+	select(Day, Time, n)
+	head(chartData)
+	ggplot(data = chartData, aes(x = Time, y = n)) + 
+	  geom_line(aes(group=Day, color = Day))+
+	  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank()) +
+	  theme(plot.title = element_text(hjust = 0.5 ,face = "bold")) + 
+	  scale_x_discrete(breaks = chartData$Time[seq(1, 96, by = 4)]) +
+	  ggtitle("Volume by day")
+
+  })  
 }
